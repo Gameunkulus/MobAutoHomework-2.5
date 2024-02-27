d11abe9af1cc0069f0458cb30bca50fa5930f987
Gameunkulus <bugr24-8@mail.ru>
2024-02-27 10:32:14 +0300

    program redacted

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/HomeworkTestClass.java
@@ -0,0 +1,39 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static java.util.regex.Pattern.matches;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.ViewInteractionComponent;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class HomeworkTestClass {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testName() {
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home)
+        );
+        mainText.check(
+                matches(
+                        withText("This is home fragment",mainText)
+                )
+        );
+    }
+}
